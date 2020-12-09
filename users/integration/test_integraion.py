6adad2899140a69ed876ea9d26c42a4c7b1ce7a4
Vadim Vadimov <berendeyzhiv@gmail.com>
2020-12-09 03:22:18 +0300

    Added performance tests

--- users/integration/test_integraion.py
+++ users/integration/test_integraion.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+from django.urls import reverse
 from django.test import TestCase, tag
 from django.contrib.auth import get_user_model
 
@@ -41,22 +42,51 @@ class IntegraionTest(TestCase):
             cls.projects.append(project)
         
 
-    def test_add_people_to_projects(self):
-        """Add 2 bosses and 9 employees to each project"""
-        
-        def self_employees_pop():
+    def test_querysets_for_the_index_page(self):
+        """SELECT * FROM boss INNER JOIN user; SELECT * FROM project"""
+        with self.assertNumQueries(2):
+            response = self.client.get('/')
+        self.assertEqual(response.context['boss_list'].count(), 10)
+        self.assertEqual(response.context['project_list'].count(), 5)
+        self.assertTemplateUsed(response, 'users/index.html')
+
+
+    def test_querysets_for_the_projects_detail(self):
+        f"""Add 2 bosses and 9 employees to each of 5 projects and
+            get 6 queries for each iteration(project): \n{sql}
+
+        """
+        def add_employee():
             return self.employees.pop()
 
         for project in self.projects:
             project.heads.add(*[self.bosses.pop(), self.bosses.pop()])
-            project.employees.add(*[self_employees_pop() for _ in range(9)])
+            project.employees.add(*[add_employee() for _ in range(9)])
+
+        two_bosses_in_project = random.choice(self.projects).heads.count()
+        nine_employees_in_project = random.choice(self.projects).employees.count()
+        self.assertEqual(two_bosses_in_project, 2)
+        self.assertEqual(nine_employees_in_project, 9)
+        
+        with self.assertNumQueries(30):
+            for project in self.projects:
+                path = reverse('users:project-detail', kwargs={'slug': project.slug})
+                response = self.client.get(path)
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(response.context['boss_list'].count(), 2)
+                self.assertEqual(response.context['employee_list'].count(), 9)
+
+        self.assertTemplateUsed(response, 'users/project_detail.html')
+        
 
 
-# ----------------------------------------------------------------------
-# Ran 1 test in 20.083s
-
-# OK
-
-# NOTE: тест показал сломанное отношение m2m 
-# through удалено
-# on_delete.CASCADE пересмотреть
+# наличие 2го SELECT я не понимаю
+sql = """
+1. SELECT * FROM "project" WHERE "project"."slug" = 'sxy' LIMIT 21
+2. SELECT * FROM "project" WHERE UPPER("project"."slug"::text) = UPPER('sxy') LIMIT 21
+3. SELECT * FROM "boss" INNER JOIN 
+    "project_heads" ON ("boss"."user_id" = "project_heads"."boss_id") WHERE "project_heads"."project_id" = 1
+4. SELECT * FROM "user" WHERE "user"."id" IN (81, 91)
+5. SELECT * FROM "employee" WHERE "employee"."project_id" = 1
+6. SELECT * FROM "user" WHERE "user"."id" IN (96, 97, 98, 99, 100, 92, 93, 94, 95)
+"""
